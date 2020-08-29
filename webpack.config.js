3434b07a94f65fcd657a373b9b8d24c894005fdd
IREZON <elvisrugamba@gmail.com>
2020-08-29 05:38:30 +0200

    Fix relative path after build

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, "/dist"),
     filename: "index_bundle.js",
-    publicPath: "/"
+    publicPath: "./"
   },
   devServer: {
     historyApiFallback: true,
