ba57cb322a76b0598eb76344ffbbf5ff0bb36c81
IREZON <elvisrugamba@gmail.com>
2020-08-29 04:40:07 +0200

    Fix routing problems

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -294,7 +294,7 @@ const Dashboard = () => {
               setOpenNewsModal(true);
               console.log("state", openNewsModal, openUserModal);
             }}
-            style={{ display: window.location.pathname === "/" ? "none" : "" }}
+            style={{ display: window.location.pathname === "/admin/" ? "none" : "" }}
           />
         </main>
       </div>
