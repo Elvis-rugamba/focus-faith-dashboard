ce9ea9ed597d59cb0fe2f948e1e246f53cf1c96e
IREZON <elvisrugamba@gmail.com>
2020-09-09 10:54:14 +0200

    Fix webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   devServer: {
     historyApiFallback: true,
     port: 9000,
-    contentBase: path.join(__dirname, 'dist'),
+    contentBase: './',
   },
   module: {
     rules: [
